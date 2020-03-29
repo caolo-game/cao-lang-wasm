1599af4de9e47723fc14d98f0accabe822703bbf
Daniel Kiss <littlesnorrboy@gmail.com>
2020-03-29 17:44:48 +0200

    Add simple funcionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-mod utils;
-
 use serde_derive::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
@@ -26,13 +21,55 @@ pub struct AstNode {
 #[wasm_bindgen(js_class=AstNode)]
 impl AstNode {
     #[wasm_bindgen(constructor)]
-    pub fn new(instruction: JsValue) -> Self {
+    pub fn new(instruction: JsValue) -> AstNode {
         let instruction = instruction
             .into_serde()
             .expect("Expected InstructionNode instance");
-        Self {
+        let node = Self {
             instruction,
             child: None,
+        };
+        node
+    }
+}
+
+impl AstNode {
+    pub fn instruction(&self) -> &cao_lang::compiler::InstructionNode {
+        &self.instruction
+    }
+}
+
+#[wasm_bindgen(js_name=CompilationUnit, inspectable)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CompilationUnit {
+    inner: cao_lang::compiler::CompilationUnit,
+}
+
+#[wasm_bindgen(js_class=CompilationUnit)]
+impl CompilationUnit {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self{
+            inner: cao_lang::compiler::CompilationUnit::default()
         }
     }
+
+    #[wasm_bindgen]
+    pub fn set_node(&mut self, id: i32, node: &AstNode) -> Result<(), JsValue> {
+        use cao_lang::compiler;
+        let node = compiler::AstNode {
+            node: node.instruction().clone(),
+            child: node.child,
+        };
+        self.inner.nodes.insert(id, node);
+        Ok(())
+    }
+}
+
+#[wasm_bindgen]
+pub fn compile(compilation_unit: &CompilationUnit) -> Result<(), JsValue> {
+    let cu = compilation_unit.inner.clone();
+    cao_lang::compiler::Compiler::compile(cu)
+        .map_err(|e| e.into())
+        .map(|_| ())
 }
--- tests/web.rs
+++ tests/web.rs
@@ -7,7 +7,29 @@ use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+use cao_lang_wasm::{compile, AstNode, CompilationUnit};
+use wasm_bindgen::JsValue;
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn can_compile_simple_program() {
+    let start_node: serde_json::Value = serde_json::json! {{
+        "Start": null
+    }};
+    let start_node = JsValue::from_serde(&start_node).unwrap();
+    let mut start_node = AstNode::new(start_node);
+    start_node.child = Some(1);
+
+    let scalar_node: serde_json::Value = serde_json::json! {{
+        "ScalarInt": {
+          "value": 69
+        }
+    }};
+    let scalar_node = JsValue::from_serde(&scalar_node).unwrap();
+    let scalar_node = AstNode::new(scalar_node);
+
+    let mut cu: CompilationUnit = CompilationUnit::new();
+    cu.set_node(0, &start_node);
+    cu.set_node(1, &scalar_node);
+
+    compile(&cu).unwrap();
 }
